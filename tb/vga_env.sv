2f2817279ba5b13550ee6dbcdf21e97d87a79b1a
xshangyong <282984685@qq.com>
2017-03-01 01:31:32 +0800

    edit vga monitor , ok

--- tb/vga_env.sv
+++ tb/vga_env.sv
@@ -7,7 +7,6 @@
 class vga_env extends uvm_env;
 
 	protected virtual interface vga_interface vif_vga;
-
 	// Control properties
 	protected int imgs_per_sec = 30;
 	protected int img_row_size = 600;
@@ -32,6 +31,28 @@ class vga_env extends uvm_env;
 			`uvm_fatal("NOVIF",{"virtual interface must be set for: ",get_full_name(),".vif"});
 		vga_agent0 = vga_agent::type_id::create("vga_agent0", this);
 		vga_agent0.is_active = UVM_PASSIVE;
+		uvm_config_db#(int)::set(this, "vga_agent0.*", "v_syn", 4);
+		uvm_config_db#(int)::set(this, "vga_agent0.*", "v_bkporch", 23);
+		uvm_config_db#(int)::set(this, "vga_agent0.*", "v_data", 16);
+		uvm_config_db#(int)::set(this, "vga_agent0.*", "v_ftporch", 1);
+		uvm_config_db#(int)::set(this, "vga_agent0.*", "v_total", 44);
+		uvm_config_db#(int)::set(this, "vga_agent0.*", "h_syn", 128);
+		uvm_config_db#(int)::set(this, "vga_agent0.*", "h_bkporch", 88);
+		uvm_config_db#(int)::set(this, "vga_agent0.*", "h_data", 800);
+		uvm_config_db#(int)::set(this, "vga_agent0.*", "h_ftporch", 40);
+		uvm_config_db#(int)::set(this, "vga_agent0.*", "h_total", 1056);
+//		parameter H_SYN 		= 128;
+//		parameter H_BKPORCH 	= 88;	
+//		parameter H_DATA 		= 800;
+//		parameter H_FTPORCH		= 40;
+//		parameter H_TOTAL    	= 1056;
+//		
+//		
+//		parameter V_SYN 		= 4;
+//		parameter V_BKPORCH 	= 23;
+//		parameter V_DATA 		= 16;
+//		parameter V_FTPORCH		= 1;
+//		parameter V_TOTAL    	= 44;	
 	endfunction : build_phase
 endclass : vga_env
 
--- tb/vga_monitor.sv
+++ tb/vga_monitor.sv
@@ -12,7 +12,25 @@ class vga_monitor extends uvm_monitor;
 	int 		frame_cnt=0;
 	bit [15:0]	data_tmp[$];
 	int			size;
-	typedef enum   {INIT=0, VSYN_H, VSYN_L_RL, VSYN_L_RH} vga_state;
+	
+	int 	h_syn 			;
+	int 	h_bkporch 	;
+	int 	h_data 			;
+	int 	h_ftporch	;
+	int 	h_total    		;
+
+	int 	v_syn 		;
+	int 	v_bkporch 	;
+	int 	v_data 		;
+	int 	v_ftporch	;
+	int 	v_total    	;	
+
+	
+	
+	
+	
+	
+	typedef enum   {INIT=0, VSYN_L, VSYN_H_RL, VSYN_H_RH} vga_state;
 	vga_state c_state = INIT;
 	protected virtual vga_interface vif_vga;
 	`uvm_component_utils(vga_monitor)
@@ -29,26 +47,39 @@ class vga_monitor extends uvm_monitor;
     super.build_phase(phase);
 	if(!uvm_config_db#(virtual vga_interface)::get(this, "", "vif_vga", vif_vga))
 		`uvm_fatal("NOVIF",{"virtual interface must be set for: ",get_full_name(),".vif"});
+		uvm_config_db#(int)::get(null, get_full_name(), "v_syn", 		v_syn);
+		uvm_config_db#(int)::get(null, get_full_name(), "v_bkporch", 	v_bkporch);
+		uvm_config_db#(int)::get(null, get_full_name(), "v_data", 		v_data);
+		uvm_config_db#(int)::get(null, get_full_name(), "v_ftporch", 	v_ftporch);
+		uvm_config_db#(int)::get(null, get_full_name(), "v_total", 		v_total);
+		uvm_config_db#(int)::get(null, get_full_name(), "h_syn", 		h_syn);
+		uvm_config_db#(int)::get(null, get_full_name(), "h_bkporch", 	h_bkporch);
+		uvm_config_db#(int)::get(null, get_full_name(), "h_data",	 	h_data);
+		uvm_config_db#(int)::get(null, get_full_name(), "h_ftporch", 	h_ftporch);
+		uvm_config_db#(int)::get(null, get_full_name(), "h_total", 		h_total);
   endfunction: build_phase
 
   // run phase
   virtual task run_phase(uvm_phase phase);
+	
+	`uvm_info("monitor",$sformatf("syn=%d,v_bkporch=%d,v_data=%d,v_ftporch=%d,v_total=%d",v_syn,v_bkporch,v_data,v_ftporch,v_total), UVM_LOW);
 	fork
       collect_trans(trans_collected);
     join
   endtask : run_phase
 
   // collect_trans
+  //  BKPORCH 23 DATA 16 FTPORCH 1
 	virtual protected task collect_trans(vga_trans trans_collected);
 	`uvm_info("monitor", "COLECT TRANS", UVM_LOW);
 	forever begin
 		case (c_state) 
 			INIT: begin
 				if(vif_vga.VSYNC_Sig == 0) begin
-					c_state <= VSYN_H;
+					c_state <= VSYN_L;
 				end
 			end
-			VSYN_H: begin
+			VSYN_L: begin
 				if(trans_collected.data.size() != 0) begin
 					trans_collected.print();
 					item_collected_port.write(trans_collected);
@@ -56,53 +87,69 @@ class vga_monitor extends uvm_monitor;
 					`uvm_info("monitor", "END ONE FRAME", UVM_LOW);
 				end
 				if(vif_vga.VSYNC_Sig == 0) begin
-					c_state <= VSYN_H;
+					c_state <= VSYN_L;
 				end
 				else begin
-					c_state <= VSYN_L_RL;
+					c_state <= VSYN_H_RL;
 					frame_cnt <= frame_cnt + 1;
 				end
 			end
-			VSYN_L_RL: begin
+			VSYN_H_RL: begin
 				if(vif_vga.VSYNC_Sig == 1 && vif_vga.HSYNC_Sig == 0) begin
-					c_state <= VSYN_L_RL;
+					c_state <= VSYN_H_RL;
 					cnt_column <= 0;
 				end
 				else if(vif_vga.VSYNC_Sig == 1 && vif_vga.HSYNC_Sig == 1) begin
-					c_state <= VSYN_L_RH;
-					cnt_column <= cnt_column + 1;
-					data_tmp.push_back(vif_vga.vga_data[15:0]) ;
+					c_state <= VSYN_H_RH;
+					if(cnt_row >= v_bkporch && cnt_row < v_bkporch + v_data && cnt_column >= h_bkporch && cnt_column < h_bkporch + h_data)begin
+						data_tmp.push_back(vif_vga.vga_data[15:0]) ;
+					end		
 					idx <= idx + 1;
+					cnt_column <= cnt_column + 1;
 				end
 				else if(vif_vga.VSYNC_Sig == 0)begin
-					c_state <= VSYN_H;
+					c_state <= VSYN_L;
 					cnt_row <= 0;
 					idx <= 0;
 					trans_collected.row_size <= cnt_row;
-					begin
 						trans_collected.size = data_tmp.size();
 						trans_collected.data = new[trans_collected.size];
 						for(int i=0;i<trans_collected.size;i++) begin
 							trans_collected.data[i] = data_tmp.pop_front();
 						end
-					end
-					`uvm_info("monitor",$sformatf("END OF L L , size = %d",size), UVM_LOW); 
+					`uvm_info("monitor",$sformatf("END OF L L , size = %d",trans_collected.size), UVM_LOW); 
 				end
 			end
-			VSYN_L_RH: begin
+			VSYN_H_RH: begin
 				if(vif_vga.VSYNC_Sig == 1 && vif_vga.HSYNC_Sig == 1) begin
-					c_state <= VSYN_L_RH;
-					cnt_column <= cnt_column + 1;
-					data_tmp.push_back(vif_vga.vga_data[15:0]) ;
+					c_state <= VSYN_H_RH;
+					if(cnt_row >= 23 && cnt_row < 39 && cnt_column >= h_bkporch && cnt_column < h_bkporch + h_data)begin
+						data_tmp.push_back(vif_vga.vga_data[15:0]) ;
+					end		
 					idx <= idx + 1;
+					cnt_column <= cnt_column + 1;
 				end
 				else if(vif_vga.VSYNC_Sig == 1 && vif_vga.HSYNC_Sig == 0) begin
-					c_state <= VSYN_L_RL;
+					c_state <= VSYN_H_RL;
 					cnt_column <= 0;
 					cnt_row <= cnt_row + 1;
-					trans_collected.column_ar.push_back(cnt_column);
-					trans_collected.column_size <= cnt_column;
-					`uvm_info("monitor",$sformatf("END, REF, size = %d",data_tmp.size()), UVM_LOW);
+					if(cnt_row >= 23 && cnt_row < 39)begin
+						trans_collected.column_ar.push_back(cnt_column);
+						trans_collected.column_size <= cnt_column;
+						`uvm_info("monitor",$sformatf("END, REF, size = %d",data_tmp.size()), UVM_LOW);
+					end
+				end
+				else if(vif_vga.VSYNC_Sig == 0) begin
+					c_state <= VSYN_L;
+					cnt_row <= 0;
+					idx <= 0;
+					trans_collected.row_size <= cnt_row;
+						trans_collected.size = data_tmp.size();
+						trans_collected.data = new[trans_collected.size];
+						for(int i=0;i<trans_collected.size;i++) begin
+							trans_collected.data[i] = data_tmp.pop_front();
+						end
+					`uvm_info("monitor",$sformatf("END OF L L , size = %d",trans_collected.size), UVM_LOW); 
 				end
 			end
 		endcase
